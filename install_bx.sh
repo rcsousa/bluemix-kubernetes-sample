1bfa4a587e6eaa7db5b4c9f5f6dab2858ac04d61
Ricardo Coelho de Sousa <rcsousa@users.noreply.github.com>
2018-11-12 07:42:57 -0200

    Update install_bx.sh

--- install_bx.sh
+++ install_bx.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 echo "Download Bluemix CLI"
-wget --quiet --output-document=/tmp/Bluemix_CLI_amd64.tar.gz  http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/latest/Bluemix_CLI_amd64.tar.gz
-tar -xf /tmp/Bluemix_CLI_amd64.tar.gz --directory=/tmp
+#wget --quiet --output-document=/tmp/Bluemix_CLI_amd64.tar.gz  http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/latest/Bluemix_CLI_amd64.tar.gz
+wget --quiet --output-document=/tmp/idt-installer  https://raw.githubusercontent.com/IBM-Bluemix/developer-tools-installer/master/linux-installer/idt-installer
+#tar -xf /tmp/Bluemix_CLI_amd64.tar.gz --directory=/tmp
 
 # Create bx alias
-echo "#!/bin/sh" >/tmp/Bluemix_CLI/bin/bx
-echo "/tmp/Bluemix_CLI/bin/bluemix \"\$@\" " >>/tmp/Bluemix_CLI/bin/bx
-chmod +x /tmp/Bluemix_CLI/bin/*
+#echo "#!/bin/sh" >/tmp/Bluemix_CLI/bin/bx
+#echo "/tmp/Bluemix_CLI/bin/bluemix \"\$@\" " >>/tmp/Bluemix_CLI/bin/bx
+#chmod +x /tmp/Bluemix_CLI/bin/*
+chmod +x /tmp/idt-installer
 
-export PATH="/tmp/Bluemix_CLI/bin:$PATH"
+export PATH="/tmp/:$PATH"
 
 # Install Armada CS plugin
-echo "Install the Bluemix container-service plugin"
-bx plugin install container-service -r Bluemix
+#echo "Install the Bluemix container-service plugin"
+#bx plugin install container-service -r Bluemix
 
-echo "Install kubectl"
-wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x /tmp/Bluemix_CLI/bin/kubectl
+#echo "Install kubectl"
+#wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+#chmod +x /tmp/Bluemix_CLI/bin/kubectl
+
+/bin/bash /tmp/idt-installer
 
 if [ -n "$DEBUG" ]; then
   bx --version
